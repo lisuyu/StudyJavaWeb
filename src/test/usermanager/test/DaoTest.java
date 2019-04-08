4974d107b29f88a616fd674ce9acb50443db9d6b
lisuyulisuyu <2534290039@qq.com>
2019-04-08 10:14:15 +0800

    init

--- src/test/usermanager/test/DaoTest.java
+++ src/test/usermanager/test/DaoTest.java
@@ -6,8 +6,13 @@ import test.usermanager.model.User;
 public class DaoTest {
     public static void main(String[] args) {
         UserDao ud = new UserDao();
-        User user = ud.load(11);
-        user.setNickname("傻狍子");
-        ud.update(user);
+//        User user = ud.load(11);
+//        user.setNickname("傻狍子");
+//        ud.update(user);
+        User user = new User();
+        user.setUsername("高文");
+        user.setPassword("321654");
+        user.setNickname("老粽子");
+        ud.add(user);
     }
 }
